96d61906860c137409d247600d4e8cbf0d14acc2
caualcruz <caualucas34492@gmail.com>
2025-08-20 21:19:01 -0300

    Atividades - 20/08

---
+++ AtividadesEmSala/src/AtividadeSala04.java
@@ -0,0 +1,11 @@
+public class AtividadeSala04 {
+    public static void  main (String[] args){
+        int x = 0;
+        x += 1;
+        System.out.println("X += 1: " + x);
+        x++;
+        System.out.println("X ++: " + x);
+        x-=2;
+        System.out.println("X -= 1: " + x);
+    }
+}
