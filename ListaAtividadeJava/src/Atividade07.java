654b29df2aeeef5bc1e8bb8af4f24be18ff7756d
caualcruz <caualucas34492@gmail.com>
2025-08-28 20:59:48 -0300

    Atividade07

---
+++ ListaAtividadeJava/src/Atividade07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Atividade07 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int a = 0, b = 0;
+        int soma = 0;
+        System.out.println("Digite os valores para a soma");
+
+        a = leia.nextInt();
+        b = leia.nextInt();
+
+        soma = a + b;
+        System.out.println("A soma dos números escolhidos é igual a: " + soma);
+    }
+}
