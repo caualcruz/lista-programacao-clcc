53b4515d52706cf110206080cd0ed6f11a68b91b
caualcruz <caualucas34492@gmail.com>
2025-08-28 19:43:42 -0300

    Atividade03

---
+++ ListaAtividadeJava/src/Atividade03.java
@@ -0,0 +1,13 @@
+public class Atividade03 {
+    public static void main(String[] args) {
+        int qtdAlunos = 100;
+        double mediaTurma = 100.0;
+        boolean aprovada = true;
+        String professor = "Vitão do grau";
+
+        System.out.println("Quantidade de alunos = " + qtdAlunos + "\n"+
+                "Média da turma = " + mediaTurma + "\n"+
+                "Aprovados = " + aprovada + "\n"+
+                "Professor = " + professor + "\n");
+    }
+}
