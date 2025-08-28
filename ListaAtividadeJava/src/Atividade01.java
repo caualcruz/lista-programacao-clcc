bc1d4ea463fac1e8c3edc5b4f10067cb27dc90d0
caualcruz <caualucas34492@gmail.com>
2025-08-28 19:36:34 -0300

    Atividade02

--- ListaAtividadeJava/src/Atividade01.java
+++ ListaAtividadeJava/src/Atividade01.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Atividade01 {
+    public static void main(String[] args) {
+        String nome = "Cauã Cruz", cidade = "SGRA";
+        final String PAIS = "Brasil";
+
+        System.out.println("Olá, me chamo " + nome + " e moro em " + cidade + "/" + PAIS);
+    }
 }
--- ListaAtividadeJava/src/Atividade02.java
+++ ListaAtividadeJava/src/Atividade02.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Atividade02 {
+    public static void main(String[] args) {
+        String dia = "26";
+        String mes = "08";
+        String ano = "2006";
+
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+
+    }
 }
